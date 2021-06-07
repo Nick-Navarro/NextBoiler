2bb7a34f072aa8d242df755fb4146d50b6697ec6
Nick Navarro <nicknavarro@jferrer-lt.therapeuticsmd.com>
2021-06-07 10:51:11 -0400

    fix(aws): aws build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import Router, { useRouter } from 'next/router'
 import NProgress from 'nprogress'
 import '@/styles/generals/global.scss'
 import { LOCALE_COPY } from 'public/locales'
-import awsExports from '../aws-exports'
+import awsmobile from '../aws-exports'
 
 // import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
 // // export const client = new AWSAppSyncClient({
@@ -18,7 +18,7 @@ import awsExports from '../aws-exports'
 // //   }
 // // });
 // Page Loader Config
-Amplify.configure({ ...awsExports, ssr: true })
+Amplify.configure({ ...awsmobile, ssr: true })
 NProgress.configure({ showSpinner: false })
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
